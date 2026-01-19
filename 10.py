60d68e1790ea8608300607f2655c6d8345319699
AnnaBelkina70191980 <anna.baltz@mail.ru>
2026-01-19 22:15:37 +0300

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, roc_curve, auc
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.utils import to_categorical
+
 #Поиск аномалий
 from sklearn.ensemble import IsolationForest
 
